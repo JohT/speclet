4db8dd6806a4ef449a75b5e86b1cbffe96be6282
JohT <johnnyt@gmx.at>
2022-02-26 16:07:47 +0100

    move simple getter functions into header

--- src/data/SpectralDataInfo.cpp
+++ src/data/SpectralDataInfo.cpp
@@ -27,10 +27,11 @@ SpectralDataInfo::SpectralDataInfo(
     if (timeResolution > Transformation::TIME_RESOLUTION_LIMIT) {
         timeResolution = Transformation::TIME_RESOLUTION_LIMIT;
     }
+    const auto minFrequencyPartitionSize = 0.0000001;
 
     jassert(newSamplingRate > 100);
     jassert(newFrequencyResolution >= 1);
-    jassert(frequencyPartitionSize > 0.0000001);
+    jassert(frequencyPartitionSize > minFrequencyPartitionSize);
 
     DBG("SpectralDataInfo constructed with resolution=" +
         juce::String(newResolution) +
@@ -39,41 +40,7 @@ SpectralDataInfo::SpectralDataInfo(
         ",partition size=" + juce::String(frequencyPartitionSize));
 }
 
-auto SpectralDataInfo::getSamplingFrequency() -> double {
-    return samplingFrequency;
-}
-
-auto SpectralDataInfo::getMaxFrequency() -> double {
-    return maxFrequency;
-}
-
-auto SpectralDataInfo::getResolution() -> long {
-    return resolution;
-}
-
-//gets the spectral line count within mSamplingFrequency/2 Hz
-auto SpectralDataInfo::getFrequencyResolution() -> long {
-    return frequencyResolution;
-}
-
-//gets the spectral line count within 1/mResolution ms
-auto SpectralDataInfo::getTimeResolution() -> long {
-    return timeResolution;
-}
-
-//gets the time resolution in ms (best resolution in case of non lin time res.)
-auto SpectralDataInfo::getTimeResolutionMs() -> double {
-    return timeResolutionMs;
-}
-
-//gets the freq. partition (e.g. 1/32 -> partition [Hz] = mSamplingFrequency/2 split into 1/32)
-auto SpectralDataInfo::getFrequencyPartitionSize() -> double {
-    return frequencyPartitionSize;
-}
-
-auto SpectralDataInfo::getSpectralLineFrequencyPartitionSize(long spectralLineNr) -> double {
-    //e.g. FFT mFrequencyPartitionSize=1/16: 1/16, 1/16, 1/16....
-    //e.g. DWT mFrequencyPartitionSize=1/16, Progression=2: 1/16, 1/16, 1/8, 1/4, 1/2
+auto SpectralDataInfo::getSpectralLineFrequencyPartitionSize(long spectralLineNr) -> const double {
     jassert(spectralLineNr >= 0);
     return frequencyPartitionSize;
 }
