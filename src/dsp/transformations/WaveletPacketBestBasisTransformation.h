088c982b42eb712bc554d587f9a824315f3d1c58
JohT <johnnyt@gmx.at>
2022-02-26 16:05:52 +0100

    move custom wave++ library changes to caller class

--- src/dsp/transformations/WaveletPacketBestBasisTransformation.h
+++ src/dsp/transformations/WaveletPacketBestBasisTransformation.h
@@ -17,17 +17,43 @@
 #include "../../plugin/SpectronParameters.h"
 #include "AbstractWaveletTransformation.h"
 
-
 class WaveletPacketBestBasisTransformation : public AbstractWaveletTransformation {
 public:
-	WaveletPacketBestBasisTransformation(
-		double samplingRate, 
-		long resolution, 
-		int windowFunctionNr		= SpectronParameters::WINDOWING_DEFAULT,
-		int waveletBaseTypeNr	= SpectronParameters::WAVELET_DEFAULT 
-	);
-	virtual ~WaveletPacketBestBasisTransformation(void);
+    WaveletPacketBestBasisTransformation(
+            double samplingRate,
+            long resolution,
+            int windowFunctionNr = SpectronParameters::WINDOWING_DEFAULT,
+            int waveletBaseTypeNr = SpectronParameters::WAVELET_DEFAULT);
+    ~WaveletPacketBestBasisTransformation() override;
 
 protected:
-	virtual void calculate();		
+    void calculate() override;
+
+private:
+    //advanced cost function type with param factor
+    using costFunAdv = real_number (WaveletPacketBestBasisTransformation::*)(const real_number *, const integer_number &, const real_number &, const real_number &, const integer_number &);
+
+    // using functional F  calculate costs from given tree A.
+    // assumption: on input B is default empty tree
+    // on output B filled up with costs and B.maxlevel=A.maxlevel
+    // advanced function to integrate the cost function used in wave++ demo "demoWavPack.cpp"
+    void getCosts(const ArrayTreePer &a, Tree &t, costFunAdv f, const real_number &sigma, const real_number &factor);
+
+    // utility
+    // advanced function to integrate the cost function used in wave++ demo "demoWavPack.cpp"
+    void getCostsHelp(const ArrayTreePer &a, Node<real_number> **ptr,
+                      costFunAdv f, const real_number &sigma, const real_number &factor,
+                      const integer_number &l, const integer_number &b);
+
+    // wrapping function: start with array bin tree A, calculate costs
+    // using F and sigma. Temporarily store costs in a bin tree and then
+    // find best basis which minimizes cost. On input H is an empty hedge,
+    // on output H completely filled up and contains the best basis
+    // advanced function to integrate the cost function used in wave++ demo "demoWavPack.cpp"
+    void extractBestBasis(const ArrayTreePer &a, HedgePer &h, const double &sigma, const double &factor);
+
+    // advanced function to integrate the cost function used in "demoWavPack.cpp"
+    // and former defined in wave++ demo "demoTools.cpp"
+    auto oracCostAdv(const real_number *data, const integer_number &n,
+                     const real_number &sigma, const real_number &factor, const integer_number &k) -> real_number;
 };
