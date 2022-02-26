4a0bc66881c54b5746e4f4c2ec638bafc275b92c
JohT <johnnyt@gmx.at>
2022-02-26 16:07:47 +0100

    move simple getter functions into header

--- src/data/SpectralDataInfo.h
+++ src/data/SpectralDataInfo.h
@@ -24,23 +24,47 @@ public:
             long frequencyResolution = 0,
             long timeResolution = 1,
             double frequencyPartitionRatio = 0.0);
-    ~SpectralDataInfo();
+
+    SpectralDataInfo() = delete; // no default constructor
+    ~SpectralDataInfo() = default;// default destructor
+
     auto operator==(SpectralDataInfo &compareObject) -> bool;
 
-    auto getSamplingFrequency() -> double;
-    auto getMaxFrequency() -> double;
-    auto getResolution() -> long;
-    auto getFrequencyResolution() -> long;     //spectral line count within mSamplingFrequency/2 Hz ("vertical resolution")
-    auto getTimeResolution() -> long;          //spectral line count within 1/mResolution ms ("horizontal resolution")
-    auto getTimeResolutionMs() -> double;      //time resolution in ms (best resolution in case of non lin time res.)
-    auto getFrequencyPartitionSize() -> double;//(e.g. 1/32 -> partition [Hz] = mSamplingFrequency/2 split into 1/32)
-    auto getSpectralLineFrequencyPartitionSize(long spectralLineNr) -> double;
+    [[nodiscard]] auto getSamplingFrequency() const -> double {
+        return samplingFrequency;
+    }
+    [[nodiscard]] auto getMaxFrequency() const -> double {
+        return maxFrequency;
+    }
+    [[nodiscard]] auto getResolution() const -> long {
+        return resolution;
+    }
+    //Gets the spectral line count within mSamplingFrequency/2 Hz ("vertical resolution")
+    [[nodiscard]] auto getFrequencyResolution() const -> long {
+        return frequencyResolution;
+    }
+    //Gets the spectral line count within 1/mResolution ms ("horizontal resolution")
+    [[nodiscard]] auto getTimeResolution() const -> long {
+        return timeResolution;
+    }
+    //Gets the time resolution in ms (best resolution in case of non linear time resolution)
+    [[nodiscard]] auto getTimeResolutionMs() const -> double {
+        return timeResolutionMs;
+    }
+    //Gets the frequency partition (e.g. 1/32 -> partition [Hz] = mSamplingFrequency/2 split into 1/32)
+    [[nodiscard]] auto getFrequencyPartitionSize() const -> double {
+        return frequencyPartitionSize;
+    }
+    //Gets the relative frequency partition size for the specified spectral line number.
+    //Example 1: Fast Fourier Transformation FFT: mFrequencyPartitionSize=1/16: 1/16, 1/16, 1/16....
+    //Example 2: Discrete (Decimated) Wavelet Transform DWT: mFrequencyPartitionSize=1/16, Progression=2: 1/16, 1/16, 1/8, 1/4, 1/2
+    auto getSpectralLineFrequencyPartitionSize(long spectralLineNr) -> const double;
 
 private:
     SpectralDataInfo(const SpectralDataInfo &) = default;                    // no copy constructor
-	auto operator=(const SpectralDataInfo &) -> SpectralDataInfo & = default; // no assignment operator
-	SpectralDataInfo(SpectralDataInfo &&) = default;                         // no move constructor
-    SpectralDataInfo() = default;                                            // no default constructor
+    auto operator=(const SpectralDataInfo &) -> SpectralDataInfo & = default;// no assignment operator
+    SpectralDataInfo(SpectralDataInfo &&) = default;                         // no move constructor
+    auto operator=(SpectralDataInfo &&) -> SpectralDataInfo & = default;     // no move assignment operator
 
     double samplingFrequency;
     long resolution;
