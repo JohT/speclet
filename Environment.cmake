459ada457f2c60acc6570d0837c7dec891a64f2c
JohT <johnnyt@gmx.at>
2022-02-26 16:04:14 +0100

    separate environment build configuration

---
+++ Environment.cmake
@@ -0,0 +1,37 @@
+message(STATUS "Environment setup started")
+
+# Fix behavior of CMAKE_CXX_STANDARD when targeting macOS.
+if (POLICY CMP0025)
+    cmake_policy(SET CMP0025 NEW)
+endif ()
+
+# Set C++ Language Standard to C++17
+set (CMAKE_CXX_STANDARD 17)
+
+# Generates a JSON file containing compile info like include paths, defines, etc. for clangd (IDE support)
+# Reference: https://stackoverflow.com/questions/39455090/clang-tidy-cant-find-my-header-files
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+# Download "cpm", the setup-free CMake dependency management 
+# https://github.com/cpm-cmake/CPM.cmake
+set(CPM_DOWNLOAD_VERSION 0.35.0)
+set(CPM_DOWNLOAD_LOCATION "${CMAKE_BINARY_DIR}/cmake/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
+if(NOT (EXISTS ${CPM_DOWNLOAD_LOCATION}))
+    message(STATUS "Downloading CPM.cmake")
+    file(DOWNLOAD https://github.com/TheLartians/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake ${CPM_DOWNLOAD_LOCATION})
+endif()
+include(${CPM_DOWNLOAD_LOCATION})
+
+#Minimum MacOS target, set globally
+set(CMAKE_OSX_DEPLOYMENT_TARGET "10.9" CACHE STRING "Minimum OS X deployment version" FORCE)
+
+option(UniversalBinary "Build universal binary for mac" OFF)
+
+if (UniversalBinary)
+    set(CMAKE_OSX_ARCHITECTURES "x86_64;arm64" CACHE INTERNAL "")
+endif()
+
+#static linking in Windows
+set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
+
+message(STATUS "Environment setup finished")
