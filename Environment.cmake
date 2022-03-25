23e2cc4a57f9bfec9e1da3533744beaf978ab800
JohT <johnnyt@gmx.at>
2022-03-25 16:16:59 +0100

    enhance build configuration for testing

--- Environment.cmake
+++ Environment.cmake
@@ -5,8 +5,8 @@ if (POLICY CMP0025)
     cmake_policy(SET CMP0025 NEW)
 endif ()
 
-# Set C++ Language Standard to C++17
-set (CMAKE_CXX_STANDARD 20)
+# Set C++ Language Standard to C++20
+set(CMAKE_CXX_STANDARD 20)
 
 # Generates a JSON file containing compile info like include paths, defines, etc. for clangd (IDE support)
 # Reference: https://stackoverflow.com/questions/39455090/clang-tidy-cant-find-my-header-files
@@ -18,12 +18,12 @@ set(CPM_DOWNLOAD_VERSION 0.35.0)
 set(CPM_DOWNLOAD_LOCATION "${CMAKE_BINARY_DIR}/cmake/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
 if(NOT (EXISTS ${CPM_DOWNLOAD_LOCATION}))
     message(STATUS "Downloading CPM.cmake")
-    file(DOWNLOAD https://github.com/TheLartians/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake ${CPM_DOWNLOAD_LOCATION})
+    file(DOWNLOAD https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake ${CPM_DOWNLOAD_LOCATION})
 endif()
 include(${CPM_DOWNLOAD_LOCATION})
 
 #Minimum MacOS target, set globally
-set(CMAKE_OSX_DEPLOYMENT_TARGET "10.9" CACHE STRING "Minimum OS X deployment version" FORCE)
+set(CMAKE_OSX_DEPLOYMENT_TARGET "10.13" CACHE STRING "Minimum OS X deployment version" FORCE)
 
 option(UniversalBinary "Build universal binary for mac" OFF)
 
