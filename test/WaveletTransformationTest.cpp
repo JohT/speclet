c2e08bd63cd538cb57802b1d79d4fae2714b11b9
JohT <johnnyt@gmx.at>
2022-03-27 18:34:52 +0200

    use distinct headers in unit test

--- test/WaveletTransformationTest.cpp
+++ test/WaveletTransformationTest.cpp
@@ -1,17 +1,18 @@
-#include "../src/PluginProcessor.h"
+// #include "../src/dsp/transformations/Transformation.h"
+// #include "../src/dsp/transformations/TransformationFactory.h"
 #include <catch2/catch_all.hpp>
 #include <string>
 
 SCENARIO("Wavelet Transformation") {
     auto samplingRate = 44100;
-    Transformation::ResolutionType resolution = 1024;
-    Transformation *transformation = TransformationFactory::getSingletonInstance().createTransformation(
-            TransformationFactory::Type::FAST_WAVELET_TRANSFORM,
-            samplingRate,
-            resolution,
-            WindowFunctionFactory::Method::RECTANGULAR,
-            AbstractWaveletTransformation::WaveletBase::VAIDYANATHAN_18);
-    REQUIRE(transformation != nullptr);
+    // Transformation::ResolutionType resolution = 1024;
+    // Transformation *transformation = TransformationFactory::getSingletonInstance().createTransformation(
+    //         TransformationFactory::Type::FAST_WAVELET_TRANSFORM,
+    //         samplingRate,
+    //         resolution,
+    //         WindowFunctionFactory::Method::RECTANGULAR,
+    //         AbstractWaveletTransformation::WaveletBase::VAIDYANATHAN_18);
+    // REQUIRE(transformation != nullptr);
     //TODO (JohT): Implement test
     GIVEN("get window") {
 
