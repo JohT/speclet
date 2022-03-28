3b4d0472e0130bc6acbf6a575c578293f891b659
JohT <johnnyt@gmx.at>
2022-03-28 18:42:49 +0200

    add wavelet transform unit tests

--- test/WaveletTransformationTest.cpp
+++ test/WaveletTransformationTest.cpp
@@ -1,23 +1,26 @@
 // #include "../src/dsp/transformations/Transformation.h"
 // #include "../src/dsp/transformations/TransformationFactory.h"
+#include "../src/dsp/transformations/WaveletTransformation.h"
+#include "catch2/matchers/catch_matchers.hpp"
 #include <catch2/catch_all.hpp>
 #include <string>
 
 SCENARIO("Wavelet Transformation") {
     auto samplingRate = 44100;
-    // Transformation::ResolutionType resolution = 1024;
-    // Transformation *transformation = TransformationFactory::getSingletonInstance().createTransformation(
-    //         TransformationFactory::Type::FAST_WAVELET_TRANSFORM,
-    //         samplingRate,
-    //         resolution,
-    //         WindowFunctionFactory::Method::RECTANGULAR,
-    //         AbstractWaveletTransformation::WaveletBase::VAIDYANATHAN_18);
-    // REQUIRE(transformation != nullptr);
-    //TODO (JohT): Implement test
-    GIVEN("get window") {
+    Transformation::ResolutionType resolution = 4096;
+    auto windowFunction = WindowFunctionFactory::Method::BLACKMAN_HARRIS;
+    auto waveletBase = AbstractWaveletTransformation::WaveletBase::VAIDYANATHAN_18;
 
-        WHEN("window functions are cached") {
-            THEN("window is newly created when resolution changes") {
+    GIVEN("Wavelet Transform is created") {
+        WaveletTransformation transformation = WaveletTransformation(samplingRate, resolution, windowFunction, waveletBase);
+        WHEN("name is requested") {
+            THEN("it should return the correct name") {
+                REQUIRE(transformation.getName() == std::string("Fast Wavelet Transformation"));
+            }
+        }
+        WHEN("window function is requested") {
+            THEN("it should return the correct window function") {
+                REQUIRE(transformation.getWindowFunction()->getName() == std::string("Blackman-Harris"));
             }
         }
     }
